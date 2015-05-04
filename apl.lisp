d8edaa19a0f7f2def3a7de3277e0c32214bc3ffa
Carlos Bartolomeu <carlosgbartolomeu@gmail.com>
2015-05-04 22:41:42 +0100

    fix print

--- apl.lisp
+++ apl.lisp
@@ -18,18 +18,20 @@
 
 (defgeneric print-tensor (arg))
 
-(defmethod print-tensor ((arg number)) 
-  (progn
-    (princ (write-to-string arg))
-    (princ " ")
+(defmethod print-tensor ((arg number))
+    (progn
+        (princ (write-to-string arg))
+        (princ " ")
     0))
 
 (defmethod print-tensor ((arg list))
-  (setq lines 0)
-	  (dolist (x arg)
-	    (let ((lines-aux (print-tensor x)))
-	      (dotimes (i lines) 
-	        (princ #\linefeed))
-	      (setq lines (+ lines-aux 1))))
-   lines
-)
+    (let ((lines 0))
+        (if arg
+            (progn
+                (setq lines (print-tensor (car arg)))
+                (if (cdr arg)
+                    (progn
+                        (dotimes (i lines)
+                            (princ #\newline))
+                        (print-tensor (cdr arg))))))
+        (+ lines 1)))
